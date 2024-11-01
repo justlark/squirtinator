83fb6ffd9ed0a066652d8d4b0a6f9f646004ef33
Lark <lark@lark.gay>
2024-11-01 08:09:45 -0400

    Add HTTP server helpers

--- src/http.rs
+++ src/http.rs
@@ -4,7 +4,7 @@ use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex as RawMutex, mut
 use esp_idf_svc::{
     hal::task::block_on,
     http::{
-        server::{Configuration, EspHttpServer},
+        server::{Configuration, Connection, EspHttpServer, Request},
         Method,
     },
     io::Write,
@@ -25,6 +25,56 @@ const HTMX: &[u8] = include_bytes!("../client/htmx.min.js.gz");
 
 const BUF_SIZE: usize = 1024;
 
+fn html_resp<C>(req: Request<C>, status: u16, body: impl AsRef<[u8]>) -> anyhow::Result<()>
+where
+    C: Connection,
+    C::Error: std::error::Error + Send + Sync + 'static,
+{
+    req.into_response(status, None, &[("Content-Type", "text/html")])?
+        .write_all(body.as_ref())?;
+
+    Ok(())
+}
+
+fn read_body<C>(req: &mut Request<C>) -> anyhow::Result<Vec<u8>>
+where
+    C: Connection,
+    C::Error: std::error::Error + Send + Sync + 'static,
+{
+    let mut body = Vec::new();
+    let mut buf = vec![0; BUF_SIZE];
+
+    while let Ok(len) = req.read(&mut buf) {
+        if len == 0 {
+            break;
+        }
+
+        body.extend_from_slice(&buf[..len]);
+    }
+
+    Ok(body)
+}
+
+#[derive(Debug, Deserialize)]
+struct WifiSettingsFormBody {
+    ssid: String,
+    password: String,
+}
+
+impl WifiSettingsFormBody {
+    fn save<P: NvsPartitionId>(&self, nvs: &mut EspNvs<P>) -> anyhow::Result<()> {
+        nvs.set_str("wifi.ssid", &self.ssid)?;
+
+        if !self.password.is_empty() {
+            nvs.set_str("wifi.password", &self.password)?;
+        }
+
+        log::info!("WiFi settings saved.");
+
+        Ok(())
+    }
+}
+
 pub fn serve<P>(
     config: &Config,
     wifi: Arc<Mutex<RawMutex, AsyncWifi<EspWifi<'static>>>>,
@@ -41,23 +91,9 @@ where
 
     let mut server = EspHttpServer::new(&server_config)?;
 
-    server.fn_handler("/", Method::Get, |req| -> anyhow::Result<()> {
-        let headers = [("Content-Type", "text/html")];
-
-        let mut resp = req.into_response(200, None, &headers)?;
-        resp.write_all(HTML_INDEX)?;
-
-        Ok(())
-    })?;
-
-    server.fn_handler("/settings", Method::Get, |req| -> anyhow::Result<()> {
-        let headers = [("Content-Type", "text/html")];
-
-        let mut resp = req.into_response(200, None, &headers)?;
-        resp.write_all(HTML_SETTINGS)?;
-
-        Ok(())
-    })?;
+    //
+    // Static assets
+    //
 
     server.fn_handler(
         "/assets/index.css",
@@ -88,6 +124,22 @@ where
         },
     )?;
 
+    //
+    // HTML pages
+    //
+
+    server.fn_handler("/", Method::Get, |req| -> anyhow::Result<()> {
+        html_resp(req, 200, HTML_INDEX)
+    })?;
+
+    server.fn_handler("/settings", Method::Get, |req| -> anyhow::Result<()> {
+        html_resp(req, 200, HTML_SETTINGS)
+    })?;
+
+    //
+    // API endpoints
+    //
+
     server.fn_handler(
         "/api/activate",
         Method::Post,
@@ -101,8 +153,6 @@ where
     let hostname = config.wifi.hostname.clone();
 
     server.fn_handler("/api/addr", Method::Get, move |req| -> anyhow::Result<()> {
-        let mut resp = req.into_response(200, None, &[("Content-Type", "text/html")])?;
-
         let wifi = block_on(wifi.lock());
 
         let addr = if wifi.wifi().driver().is_sta_connected()? {
@@ -111,70 +161,46 @@ where
             None
         };
 
-        let body = match addr {
-            Some(addr) => format!(
-                "
-                <p>Your Squirtinator is connected to WiFi:</p>
-                <p>
-                  http://{}.local<br />
-                  http://{}
-                </p>
-                ",
-                &hostname, addr,
-            ),
-            None => String::from(
-                "
-                <p>Your Squirtinator is not connected to WiFi.</p>
-                ",
-            ),
-        };
-
-        resp.write_all(body.as_bytes())?;
+        html_resp(
+            req,
+            200,
+            &match addr {
+                Some(addr) => format!(
+                    "
+                    <p>Your Squirtinator is connected to WiFi:</p>
+                    <p>
+                      http://{}.local<br />
+                      http://{}
+                    </p>
+                    ",
+                    &hostname, addr,
+                ),
+                None => String::from(
+                    "
+                    <p>Your Squirtinator is not connected to WiFi.</p>
+                    ",
+                ),
+            },
+        )?;
 
         Ok(())
     })?;
-
-    #[derive(Debug, Deserialize)]
-    struct WifiSettingsFormBody {
-        ssid: String,
-        password: String,
-    }
-
-    impl WifiSettingsFormBody {
-        fn save<P: NvsPartitionId>(&self, nvs: &mut EspNvs<P>) -> anyhow::Result<()> {
-            nvs.set_str("wifi.ssid", &self.ssid)?;
-
-            if !self.password.is_empty() {
-                nvs.set_str("wifi.password", &self.password)?;
-            }
-
-            log::info!("WiFi settings saved.");
-
-            Ok(())
-        }
-    }
 
     server.fn_handler(
         "/api/settings/wifi",
         Method::Put,
         move |mut req| -> anyhow::Result<()> {
-            let mut body = Vec::new();
-            let mut buf = vec![0; BUF_SIZE];
-
-            while let Ok(len) = req.read(&mut buf) {
-                if len == 0 {
-                    break;
-                }
-
-                body.extend_from_slice(&buf[..len]);
-            }
-
-            let form_body = serde_urlencoded::from_bytes::<WifiSettingsFormBody>(&body)?;
+            let req_body = read_body(&mut req)?;
+            let form_body = serde_urlencoded::from_bytes::<WifiSettingsFormBody>(&req_body)?;
 
             let mut user_nvs = user_nvs(nvs_part.clone())?;
             form_body.save(&mut user_nvs)?;
 
-            req.into_ok_response()?;
+            html_resp(
+                req,
+                200,
+                "<p>WiFi settings saved. Restart the device to connect to the new network.</p>",
+            )?;
 
             Ok(())
         },
