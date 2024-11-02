8a375f34ded4b48ff3d6ea7d00ed1c35b2f29d5a
Lark <lark@lark.gay>
2024-11-01 23:45:37 -0400

    Change connection backoff algorithm

--- src/wifi.rs
+++ src/wifi.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{cmp, time::Duration};
 
 use anyhow::anyhow;
 use esp_idf_svc::{
@@ -14,37 +14,23 @@ use esp_idf_svc::{
 
 use crate::config;
 
-// Even when users have their toy configured in station (STA) mode, we still allow them to connect
-// in access point (AP) mode so they have a way to reconfigure the SSID/password if the toy isn't
-// able to connect to the local network for whatever reason.
+// In my testing, it can sometimes take the device a few attempts to connect to the local network,
+// even with a strong signal.
 //
-// We can configure the networking stack in AP+STA mode, but I've found I'm unable to connect to
-// the toy in AP mode when it's in the process of connecting to the local network in STA mode, as
-// if the two can't happen concurrently.
+// While it's prudent to use exponential backoff, we also want to get the device connected ASAP,
+// because sex toys are a particular class of device that users have very little patience for
+// debugging (and why should they). So we make *n* "eager" attempts first, before we start applying
+// backoff.
 //
-// It appears some other folks have run into this issue:
-// https://esp32.com/viewtopic.php?t=8063
+// We also cap the maximum backoff; a mobile device such as this may be on an unstable network or
+// move in and out of range of networks, so we don't want to give up entirely.
 //
-// We could give up after a period of time and stop trying to connect, but that's an inappropriate
-// solution for a device which is mobile and could therefore come back into WiFi range at any time.
-// It's also frustrating for users, who will not have any log messages or helpful code comments to
-// explain why they can't get their sex toy to work.
-//
-// As a compromise, we first attempt to connect to the local network "eagerly", *n* times. If that
-// fails, we back off and only attempt to connect periodically, on a timer. With this solution:
-//
-// - We give the networking stack a few chances up front to set up a connection ASAP so users
-// aren't waiting.
-// - If it's not able to connect quickly enough, we open up some space for users to connect to the
-// toy in AP mode.
-// - We don't give up entirely, in case the toy is able to come back online later.
-//
-// Because this strategy is somewhat difficult to explain clearly in an annotated config file, I've
-// elected to not make these defaults configurable.
+// We need to be somewhat aggressive about connection attempts because users won't have the benefit
+// of log messages or helpful code comments to explain why they can't get their sex toy to work.
 #[derive(Debug, Clone, Copy)]
 enum ConnectStrategy {
     Eager { attempt: u32 },
-    Periodic,
+    Backoff { time: Duration },
 }
 
 impl Default for ConnectStrategy {
@@ -54,23 +40,27 @@ impl Default for ConnectStrategy {
 }
 
 impl ConnectStrategy {
-    pub const MAX_ATTEMPTS: u32 = 3;
-    pub const WAIT_DURATION: Duration = Duration::from_secs(10);
+    pub const EAGER_ATTEMPTS: u32 = 3;
+    pub const MAX_BACKOFF: Duration = Duration::from_secs(2u64.pow(4));
+    pub const BACKOFF_MULTIPLIER: u32 = 2;
 
-    pub fn attempt(&mut self) {
+    pub fn next_attempt(&mut self) {
         match self {
-            Self::Eager { attempt: attempts } if *attempts >= Self::MAX_ATTEMPTS => {
-                *self = Self::Periodic;
+            Self::Eager { attempt: attempts } if *attempts >= Self::EAGER_ATTEMPTS => {
+                *self = Self::Backoff {
+                    time: Duration::from_secs(1),
+                };
             }
             Self::Eager { attempt: attempts } => {
                 *attempts += 1;
             }
-            Self::Periodic => {}
+            Self::Backoff { time } => {
+                *time = cmp::min(*time * Self::BACKOFF_MULTIPLIER, Self::MAX_BACKOFF);
+            }
         }
     }
 }
 
-// This function doesn't return until/unless the STA-mode connection succeeds.
 pub async fn connect(
     wifi: &mut AsyncWifi<EspWifi<'static>>,
     timer_service: EspTaskTimerService,
@@ -79,21 +69,23 @@ pub async fn connect(
     let mut timer = timer_service.timer_async()?;
 
     loop {
+        strategy.next_attempt();
+
         match strategy {
             ConnectStrategy::Eager { attempt } => {
                 log::info!(
                     "Connecting to WiFI in STA mode (attempt {} of {})...",
-                    attempt + 1,
-                    ConnectStrategy::MAX_ATTEMPTS
+                    attempt,
+                    ConnectStrategy::EAGER_ATTEMPTS
                 );
             }
-            ConnectStrategy::Periodic => {
+            ConnectStrategy::Backoff { time } => {
                 log::info!(
                     "Backing off. Waiting {}s before attempting to connect...",
-                    ConnectStrategy::WAIT_DURATION.as_secs()
+                    time.as_secs()
                 );
 
-                timer.after(ConnectStrategy::WAIT_DURATION).await?;
+                timer.after(time).await?;
 
                 log::info!("Connecting to WiFI in STA mode...");
             }
@@ -102,9 +94,6 @@ pub async fn connect(
         match wifi.connect().await {
             Err(err) if err.code() == ESP_ERR_TIMEOUT => {
                 log::warn!("WiFi connection attempt timed out. Retrying...",);
-
-                strategy.attempt();
-
                 continue;
             }
             Err(err) => return Err(err.into()),
